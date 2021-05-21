606d4c619c5508474c199eaea620f929d5b1f4fc
Eric Barkuloo <jbarkuloo@ufl.edu>
2021-05-21 00:33:40 -0400

    Changed delay implementation

--- Delay_Effect/delay_plugin.m
+++ Delay_Effect/delay_plugin.m
@@ -7,19 +7,17 @@ classdef delay_plugin < audioPlugin
     type = 'basic'
     L_Gain = 1
     R_Gain = 1
-    L_Delay = 0
-    R_Delay = 0
+    L_Delay = 200
+    R_Delay = 200
     L_Feedback = 0
     R_Feedback = 0
   end
   
-  properties %(Hidden)
-    L_prev_buff_size = 0;
-    R_prev_buff_size = 0;
-    L_delay_buff = 0;
-    L_feedback_buff = 0;
-    R_delay_buff = 0;
-    R_feedback_buff = 0;
+  properties (Access = private)
+      Buffer = zeros(192001,2) %Allows up to 4 seconds of delay at 48kHz
+      Out_Buffer = zeros(192001,2) 
+      L_Index = 0;
+      R_Index = 0;
   end
   
   properties (Constant, Hidden)
@@ -46,12 +44,12 @@ classdef delay_plugin < audioPlugin
         'Style','rotary', ...
         'Layout',[3,2]), ...
     audioPluginParameter( ...
-        'L_Delay','Mapping',{'int', 0, 500}, ...
+        'L_Delay','Mapping',{'int', 0, 4000}, ...
         'DisplayName', 'Left Delay (ms)', ...
         'Style','hslider', ...
         'Layout',[5,1]), ...
     audioPluginParameter( ...
-        'R_Delay','Mapping',{'int', 0, 500}, ...
+        'R_Delay','Mapping',{'int', 0, 4000}, ...
         'DisplayName', 'Right Delay (ms)', ...
         'Style','hslider', ...
         'Layout',[5,2]), ...
@@ -77,69 +75,99 @@ classdef delay_plugin < audioPlugin
     function out = process(plugin, in)
         fs = getSampleRate(plugin);
         dry = in; %use input to get wet/dry data for mixing
-        wet = in;
 
         if(strcmp(plugin.bypass,'On'))
             out = dry;
             return
         end
 
-        left_in = in(:,1);
-        right_in = in(:,2);
-        left_out = in(:,1);
-        right_out = in(:,2);
-
         L_Delay_Samp = floor(fs*plugin.L_Delay/1000); %Convert to Samples from ms
         R_Delay_Samp = floor(fs*plugin.R_Delay/1000);
+        wet = in; %initialize wet to same dimensions as input
         
-        if(plugin.L_prev_buff_size ~= L_Delay_Samp)
-            plugin.L_delay_buff = zeros(L_Delay_Samp,1); %initialize delay buffers with 0's
-            plugin.L_feedback_buff = zeros(L_Delay_Samp,1);
-        end
-        if(plugin.R_prev_buff_size ~= R_Delay_Samp)
-            plugin.R_delay_buff = zeros(R_Delay_Samp,1); %initialize delay buffers with 0's
-            plugin.R_feedback_buff = zeros(R_Delay_Samp,1);
-        end
-        plugin.L_prev_buff_size = L_Delay_Samp;
-        plugin.R_prev_buff_size = R_Delay_Samp;
-        
-        if L_Delay_Samp == 0
-            plugin.L_delay_buff = 0;
-            plugin.L_feedback_buff = 0;
-        end
-        if R_Delay_Samp == 0
-            plugin.R_delay_buff = 0;
-            plugin.R_feedback_buff = 0;
-        end
-
         switch plugin.type
             case "basic"
-                %Basic delay implementation
-                for i = 1:length(left_in)
-                    if L_Delay_Samp == 0
-                        left_out(i) = left_in(i);
-                    else
-                        %Apply delay and update buffers for LEFT
-                        left_out(i) = left_in(i) + 10^(plugin.L_Gain/10)*plugin.L_delay_buff(L_Delay_Samp) + plugin.L_Feedback*plugin.L_feedback_buff(L_Delay_Samp);
-                        plugin.L_delay_buff = [left_in(i); plugin.L_delay_buff(1:L_Delay_Samp-1)];
-                        plugin.L_feedback_buff = [left_out(i); plugin.L_feedback_buff(1:L_Delay_Samp-1)];
+                %Basic delay implementation, based on https://www.mathworks.com/help/audio/gs/audio-plugins-in-matlab.html
+                %Left Channel
+                writeIndex = plugin.L_Index+1;
+                readIndex = writeIndex-L_Delay_Samp;
+                
+                if(readIndex <= 0) %Handle underflow of circular buffer
+                    readIndex = readIndex + length(plugin.Buffer);
+                end
+                
+                for i = 1:length(in(:,1))
+                    plugin.Buffer(writeIndex,1) = in(i,1);
+                    delay = plugin.Buffer(readIndex,1);
+                    feedback = plugin.Out_Buffer(readIndex,1);
+                    wet(i,1) = in(i,1) + 10^(plugin.L_Gain/10)*delay + plugin.L_Feedback*feedback;
+                    plugin.Out_Buffer(writeIndex,1) = wet(i,1);
+                    
+                    writeIndex = writeIndex + 1; %Handle overflow of circular buffer
+                    if(writeIndex > length(plugin.Buffer))
+                        writeIndex = 1;
                     end
-                    if R_Delay_Samp == 0
-                        right_out(i) = right_in(i);
-                    else
-                        %Apply delay and update buffers for RIGHT
-                        right_out(i) = right_in(i) + 10^(plugin.R_Gain/10)*plugin.R_delay_buff(R_Delay_Samp) + plugin.R_Feedback*plugin.R_feedback_buff(R_Delay_Samp);
-                        plugin.R_delay_buff = [right_in(i); plugin.R_delay_buff(1:R_Delay_Samp-1)];
-                        plugin.R_feedback_buff = [right_out(i); plugin.R_feedback_buff(1:R_Delay_Samp-1)];
+                    
+                    readIndex = readIndex + 1; %Handle overflow of circular buffer
+                    if(readIndex > length(plugin.Buffer))
+                        readIndex = 1;
                     end
                 end
+                plugin.L_Index = writeIndex;
+                %Right Channel
+                writeIndex = plugin.R_Index+1;
+                readIndex = writeIndex-R_Delay_Samp;
+                
+                if(readIndex <= 0) %Handle underflow of circular buffer
+                    readIndex = readIndex + length(plugin.Buffer);
+                end
+                
+                for i = 1:length(in(:,2))
+                    plugin.Buffer(writeIndex,2) = in(i,2);
+                    delay = plugin.Buffer(readIndex,2);
+                    feedback = plugin.Out_Buffer(readIndex,2);
+                    wet(i,2) = in(i,2) + 10^(plugin.R_Gain/10)*delay + plugin.R_Feedback*feedback;
+                    plugin.Out_Buffer(writeIndex,2) = wet(i,2);
+                    
+                    writeIndex = writeIndex + 1; %Handle overflow of circular buffer
+                    if(writeIndex > length(plugin.Buffer))
+                        writeIndex = 1;
+                    end
+                    
+                    readIndex = readIndex + 1; %Handle overflow of circular buffer
+                    if(readIndex > length(plugin.Buffer))
+                        readIndex = 1;
+                    end
+                end
+                plugin.R_Index = writeIndex;
+                
             case "ping-pong"
                 %Ping-Pong delay
-            otherwise
-                %By default use basic delay
+%                  for i = 1:length(left_in)
+%                     % apply delay
+%                     if L_Delay_Samp == 0
+%                         left_out(i) = left_in(i);
+%                     else
+% %                         left_out(i) = left_in(i) + 10^(plugin.L_Gain/10)*plugin.L_delay_buff(L_Delay_Samp) + plugin.L_Feedback*plugin.L_feedback_buff(L_Delay_Samp);
+%                         %update delay buffers
+% %                         plugin.L_delay_buff = [left_in(i); plugin.L_delay_buff(1:L_Delay_Samp-1)];
+%                         %feed left output to right feedback buffer and vice versa
+% %                         plugin.L_feedback_buff = [right_out(i); plugin.L_feedback_buff(1:L_Delay_Samp-1)];
+%                     end
+%                     if R_Delay_Samp == 0
+%                         right_out(i) = right_in(i);
+%                     else                        
+% %                         right_out(i) = right_in(i) + 10^(plugin.R_Gain/10)*plugin.R_delay_buff(R_Delay_Samp) + plugin.R_Feedback*plugin.R_feedback_buff(R_Delay_Samp);                            
+%                         %update delay buffers
+% %                         plugin.R_delay_buff = [right_in(i); plugin.R_delay_buff(1:R_Delay_Samp-1)];                                     
+%                         %feed left output to right feedback buffer and vice versa
+% %                         plugin.R_feedback_buff = [left_out(i); plugin.R_feedback_buff(1:R_Delay_Samp-1)];
+%                     end
+%                  end
         end
 
-        wet = [left_out,right_out];
+        %wet = [left_out,right_out];
+        
         out = (1-plugin.mix)*dry + (plugin.mix)*wet;
     end
   end
